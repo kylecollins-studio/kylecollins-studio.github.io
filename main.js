6ac6f1d1e54d732522848b769645681f8cebbf7e
Fergus Fettes <fergus.fettes@ondewo.com>
2020-11-11 21:14:17 +0100

    trying to arrange multiple images

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-var leftPath = new Path({
-    strokeColor: 'red',
-	opacity: 0.5
-});
-
-var rightPath = new Path({
-	strokeColor: 'green',
-	opacity: 0.5
-});
-
 // Create a raster item using the image tag with id='mona'
-var raster = new Raster('ferg');
+var raster_1 = new Raster('1');
+var raster_2 = new Raster('2');
+var raster_3 = new Raster('3');
+
+const rasters = [raster_1, raster_2, raster_3];
 
 // Move the raster to the center of the view
-raster.position = view.center;
 
-// Scale the raster by 50%
-raster.scale(0.5);
+rasters.forEach((raster, ndx) => {
+    const image_scale = 1 / (raster.width / view.size._width)
+    raster.scale(image_scale * 0.2);
+    raster.position = view.center;
+    raster.position._x = raster.position._x + 400;
+    debugger;
+});
 
-// Rotate the raster by 45 degrees:
-raster.rotate(45);
+// // Rotate the raster by 45 degrees:
+// raster.rotate(45);
