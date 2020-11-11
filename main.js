ef33d9fd25e55efd5112c2ce89f0d536c48d68f1
Fergus Fettes <fergus.fettes@ondewo.com>
2020-11-11 18:04:40 +0100

    fixed picture style

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var raster = new Raster('ferg');
 raster.position = view.center;
 
 // Scale the raster by 50%
-raster.scale(0.1);
+raster.scale(0.5);
 
 // Rotate the raster by 45 degrees:
 raster.rotate(45);
